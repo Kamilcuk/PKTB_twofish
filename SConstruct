63d48826929cbcc6dd8b4bf5ed0aaaf8f4ec6ca7
Kamilcuk <kamilcukrowski@gmail.com>
2016-03-11 22:19:45 +0100

    qtcreator project

--- SConstruct
+++
@@ -1,4 +0,0 @@
-# SConstruct
-env = Environment()
-env.Append(CCFLAGS = "-Wall -O2");
-twofish = Program(["main.c"])
--- main.c
+++ main.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-#include <math.h>
 
-
-int main(int argc, char* argv[])
-    {
-      printf("Hello world\n");
-      return 0;
-    }
-
-
+int main(int argc, char *argv[])
+{
+    printf("Hello World!\n");
+    return 0;
+}
