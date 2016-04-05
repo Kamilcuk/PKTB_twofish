f9552841f12e10d3e1f570c81f11f6bcee06db64
Kamilcuk <kamilcukrowski@gmail.com>
2016-04-05 12:44:38 +0200

    final final final

--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ scons > /dev/null
 
 tryby="ECB CBC PCBC CFB OFB";
 
+set -x
 for tryb in $tryby; do
         echo "## Szyfrowanie i deszyfracja trybem szyfrowania ${tryb}. Wynik operacji:"
-        set -x
 	./twofish -e -k ./klucz.txt -i ./tekst.txt            -t ${tryb} -o /tmp/tekst_${tryb}_ct.txt
 	./twofish -d -k ./klucz.txt -i /tmp/tekst_${tryb}_ct.txt -t ${tryb} -o /tmp/tekst_${tryb}.txt
-        set +x
 
 	#head -n 10 /tmp/tekst_${tryb}.txt | cut -c 1-50
 	cat /tmp/tekst_${tryb}.txt
 done
+set +x
 
